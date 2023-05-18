39b1269d0c657757519fd3ed207ea430a8b53f46
Eric Freilafert <efreilafert@cyral.com>
2023-05-17 21:57:58 -0700

    add more mac configurations

--- .chezmoiscripts/run_onchange_after_configure.sh
+++ .chezmoiscripts/run_onchange_after_configure.sh
@@ -4,9 +4,37 @@ set -eufo pipefail
 
 # https://github.com/mathiasbynens/dotfiles/blob/main/.macos
 
+# Disables natural scrolling
 defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
 
-# Dock
+###############################################################################
+# Dock                                                                        #
+###############################################################################
+
 defaults write com.apple.dock autohide -bool true
 defaults write com.apple.dock "autohide-delay" -float "0.0001"
 defaults write com.apple.dock show-recents -bool false
+
+###############################################################################
+# Screen                                                                      #
+###############################################################################
+
+# Require password immediately after sleep or screen saver begins
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 0
+
+# Use scroll gesture with the Ctrl (^) modifier key to zoom
+defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
+defaults write com.apple.universalaccess HIDScrollZoomModifierMask -int 262144
+
+###############################################################################
+# Kill affected applications                                                  #
+###############################################################################
+
+for app in "Dock" \
+	"SystemUIServer" \
+	"Terminal" \
+	"cfprefsd"; do
+	killall "${app}" &> /dev/null
+done
+echo "Done. Note that some of these changes require a logout/restart to take effect."
